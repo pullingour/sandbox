5ae1a3d2ca68399676eb2055d2f99fbb87d6b63a
pullingour <kiyomasa.sato.github@gmail.com>
2017-08-07 15:29:50 +0900

    add smilegatter

---
+++ smilegetter/app.js
@@ -0,0 +1,10 @@
+<div id="MainVideoPlayer" class="MainVideoPlayer"><video style="position: absolute; width: 100%; height: 100%; top: 0px; left: 0px; bottom: 0px; right: 0px; display: block;" crossorigin="anonymous" src="http://pa03.dmc.nico:2807/vod/ht2_nicovideo/nicovideo-sm31704938_1402da6c7799ebe3de8dc75f89528567af46e94983461ca6e6f803b47140a245?ht2_nicovideo=60479548.4ve7xe_ouatzw_16sr90xokmnmo"></video></div>
+
+
+
+function hoge() {
+  var x = document.getElementById('MainVideoPlayer')
+  console.log(x)
+}
+
+hoge()
