0627d96d5146dcc66d0bd627d94aae738d6e5606
pullingour <kiyomasa.sato.github@gmail.com>
2017-08-07 15:31:22 +0900

    ftw

--- smilegetter/app.js
+++ smilegetter/app.js
@@ -1,10 +1,25 @@
-<div id="MainVideoPlayer" class="MainVideoPlayer"><video style="position: absolute; width: 100%; height: 100%; top: 0px; left: 0px; bottom: 0px; right: 0px; display: block;" crossorigin="anonymous" src="http://pa03.dmc.nico:2807/vod/ht2_nicovideo/nicovideo-sm31704938_1402da6c7799ebe3de8dc75f89528567af46e94983461ca6e6f803b47140a245?ht2_nicovideo=60479548.4ve7xe_ouatzw_16sr90xokmnmo"></video></div>
-
-
-
-function hoge() {
-  var x = document.getElementById('MainVideoPlayer')
-  console.log(x)
+// popup version
+{
+  const x = document.getElementById('MainVideoPlayer')
+  const y = x.innerHTML
+  const z = y.toString()
+  let url = z.substring(153)
+  url = url.slice(0,-10)
+  window.open(url, '_blank')
 }
 
-hoge()
+// alert version
+{
+  const x = document.getElementById('MainVideoPlayer')
+  const y = x.innerHTML
+  const z = y.toString()
+  let url = z.substring(153)
+  alert(url.slice(0,-10))
+}
+
+// == short version ==
+// popup --short
+{const x = document.getElementById('MainVideoPlayer');const y = x.innerHTML;const z = y.toString();let url = z.substring(153);url = url.slice(0,-10);window.open(url, '_blank');}
+
+// alert --short
+{const x = document.getElementById('MainVideoPlayer');const y = x.innerHTML;const z = y.toString();let url = z.substring(153);alert(url.slice(0,-10));}
