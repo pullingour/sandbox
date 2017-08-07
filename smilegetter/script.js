2b8bf89f184b4aafb205a892de27fb40d5f96ef8
pullingour <kiyomasa.sato.github@gmail.com>
2017-08-07 19:19:46 +0900

    remove whitespaces

--- smilegetter/script.js
+++ smilegetter/script.js
@@ -1,5 +1,5 @@
 // popup version
-{const x = document.getElementById('MainVideoPlayer');const y = x.innerHTML;const z = y.toString();let url = z.substring(153);url = url.slice(0,-10);window.open(url, '_blank');}
+{const x=document.getElementById('MainVideoPlayer');const y=x.innerHTML;const z=y.toString();let url = z.substring(153);url=url.slice(0,-10);window.open(url,'_blank');}
 
 // alert version
-{const x = document.getElementById('MainVideoPlayer');const y = x.innerHTML;const z = y.toString();let url = z.substring(153);alert(url.slice(0,-10));}
+{const x=document.getElementById('MainVideoPlayer');const y=x.innerHTML;const z=y.toString();let url=z.substring(153);alert(url.slice(0,-10));}
