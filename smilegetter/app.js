8f0d01be77b495aae0d148cdd5092c3a100fb591
pullingour <kiyomasa.sato.github@gmail.com>
2017-08-07 15:39:34 +0900

    remove readable version

--- smilegetter/app.js
+++ smilegetter/app.js
@@ -1,25 +1,5 @@
 // popup version
-{
-  const x = document.getElementById('MainVideoPlayer')
-  const y = x.innerHTML
-  const z = y.toString()
-  let url = z.substring(153)
-  url = url.slice(0,-10)
-  window.open(url, '_blank')
-}
-
-// alert version
-{
-  const x = document.getElementById('MainVideoPlayer')
-  const y = x.innerHTML
-  const z = y.toString()
-  let url = z.substring(153)
-  alert(url.slice(0,-10))
-}
-
-// == short version ==
-// popup --short
 {const x = document.getElementById('MainVideoPlayer');const y = x.innerHTML;const z = y.toString();let url = z.substring(153);url = url.slice(0,-10);window.open(url, '_blank');}
 
-// alert --short
+// alert version
 {const x = document.getElementById('MainVideoPlayer');const y = x.innerHTML;const z = y.toString();let url = z.substring(153);alert(url.slice(0,-10));}
