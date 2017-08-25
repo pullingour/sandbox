2285b33c3c0f3ee3ada81c832e07711a7d7aa4ed
pullingour <kiyomasa.sato.github@gmail.com>
2017-08-26 04:08:57 +0900

    FUCK YOU

--- smilegetter/script.js
+++ smilegetter/script.js
@@ -1,4 +1,4 @@
-// <3
+// 滅びろドワンゴ
 // popup version
 {const x=document.getElementById('MainVideoPlayer');const y=x.innerHTML;const z=y.toString();let url=z.substring(153);url=url.slice(0,-10);window.open(url,'_blank');}
 
