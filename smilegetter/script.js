cf28825237307f04f77c929a3d2163a4460436fb
pullingour <kiyomasa.sato.github@gmail.com>
2017-08-26 16:11:53 +0900

    yryr

--- smilegetter/script.js
+++ smilegetter/script.js
@@ -1,6 +1,6 @@
-// 滅びろドワンゴ
+// http://smile-cln57.nicovideo.jp/smile?m=27322846.9361low
 // popup version
-{const x=document.getElementById('MainVideoPlayer');const y=x.innerHTML;const z=y.toString();let url=z.substring(153);url=url.slice(0,-10);window.open(url,'_blank');}
+{const x=document.getElementById('MainVideoPlayer');const y=x.innerHTML;const z=y.toString();let url=z.substring(129);url=url.slice(0,-10);window.open(url,'_blank');}
 
 // alert version
-{const x=document.getElementById('MainVideoPlayer');const y=x.innerHTML;const z=y.toString();let url=z.substring(153);alert(url.slice(0,-10));}
+{const x=document.getElementById('MainVideoPlayer');const y=x.innerHTML;const z=y.toString();let url=z.substring(129);alert(url.slice(0,-10));}
